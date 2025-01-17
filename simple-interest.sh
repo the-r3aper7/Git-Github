851d4f281567ed811569802a89aad9e4591c6998
Sai Srikar Dumpeti <80447788+the-r3aper7@users.noreply.github.com>
2025-01-18 01:06:16 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 principal rate time"
+    exit 1
+fi
+
+interest=$(echo "scale=2; $1 * $2 * $3 / 100" | bc)
+total=$(echo "scale=2; $1 + $interest" | bc)
+
+echo "Simple Interest: $interest"
+echo "Total Amount: $total"
